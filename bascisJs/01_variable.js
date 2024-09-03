3b0ffa70e8e20dfd6b9583a26488640adee42594
Gaurav_G6 <94691016+gairolag@users.noreply.github.com>
2024-09-03 07:01:06 +0000

    02_datatypes

---
+++ bascisJs/01_variable.js
@@ -0,0 +1,7 @@
+const accountID = 12333
+let emailId = "gaurav.gairola324@gmail.com"
+var password = "123455"
+let name 
+console.table([accountID,emailId,password])
+
+console.log(typeof (name))
---
+++ bascisJs/02_datatypes.js
@@ -0,0 +1,11 @@
+let age = 23
+let name = "yash"
+let islog = false 
+
+console.table ([age,"name",islog])
+console.log(typeof age)
+console.log(typeof "name")
+console.log(typeof islog)
+
+// null = standalone value 
+// undifined = not assing nay vaule 
